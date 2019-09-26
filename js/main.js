504a1c8d987d9b2d32bc9931989eed77f5ef9ed3
Елья <piragof@gmail.com>
2019-09-26 19:40:07 +0400

    правки

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
 var pictures = document.querySelector('.pictures');
 var template = document.querySelector('#picture').content.querySelector('.picture');
+var fragment = document.createDocumentFragment();
 
 
 var likes = {
@@ -23,10 +24,9 @@ function getRandomNumber(min, max) {
 
 
 var images = [];
-var i;
-for (i = 0; i < 25; i++) {
+for (var imageNumber = 1; imageNumber <= 25; imageNumber++) {
   images.push({
-    url: 'photos/' + (i + 1) + '.jpg',
+    url: 'photos/' + imageNumber + '.jpg',
     likes: getRandomNumber(likes.minimum, likes.maximum),
     messages: getRandomNumber(messages.minimum, messages.maximum)
   });
@@ -35,50 +35,50 @@ for (i = 0; i < 25; i++) {
 
 var renderTemplate = function () {
   var userImage = template.cloneNode(true);
+  var image = images[i];
 
-  userImage.querySelector('.picture__img').src = images[i].url;
-  userImage.querySelector('.picture__likes').textContent = images[i].likes;
-  userImage.querySelector('.picture__comments').textContent = images[i].messages;
+  userImage.querySelector('.picture__img').src = image.url;
+  userImage.querySelector('.picture__likes').textContent = image.likes;
+  userImage.querySelector('.picture__comments').textContent = image.messages;
 
   return userImage;
 };
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < images.length; i++) {
+
+for (var i = 0; i < images.length; i++) {
   fragment.appendChild(renderTemplate(images[i]));
 }
+
 pictures.appendChild(fragment);
 
-var popup = document.querySelector('.big-picture');
-var openPopupButton = document.querySelectorAll('.picture__img');
-var closePopupButton = popup.querySelectorAll('.big-picture__cancel');
 
-for (var k = 0; k < openPopupButton.length; k++) {
-  openPopupButton[k].addEventListener('click', function (e) {
+/*
+var p = function (array) {
+  var fragment = document.createDocumentFragment();
+    for (var i = 0; i < array.length; i++) {
+  fragment.appendChild(renderTemplate(array[i]));
+  }
+  return fragment
+}
+
+p(images);
+*/
+var bigPicture = document.querySelector('.big-picture');
+var openBigPictures = document.querySelectorAll('.picture__img');
+var closeBigPictures = bigPicture.querySelectorAll('.big-picture__cancel');
+
+for (var k = 0; k < openBigPictures.length; k++) {
+  openBigPictures[k].addEventListener('click', function (e) {
     e.preventDefault();
-    popup.classList.remove('hidden');
+    bigPicture.classList.remove('hidden');
   });
 }
 
-for (var j = 0; j < closePopupButton.length; j++) {
-  closePopupButton[j].addEventListener('click', function (e) {
+for (var j = 0; j < closeBigPictures.length; j++) {
+  closeBigPictures[j].addEventListener('click', function (e) {
     e.preventDefault();
     e.target.closest('.big-picture').classList.add('hidden');
   });
 }
 
 
-/*
-var comments = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-function arrayRandElement(arr) {
-  var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
-}*/
