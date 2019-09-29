9c7a6eb06e6d255d2d17c681b5ca58a0f98e9b7d
elyaperagov <piragof@gmail.com>
2019-09-29 21:54:58 +0300

    загрузка+ресайз

--- js/main.js
+++ js/main.js
@@ -69,37 +69,42 @@ for (var j = 0; j < closeBigPictures.length; j++) {
     e.target.closest('.big-picture').classList.add('hidden');
   });
 }
-
+// ЗАГРУЗКА ИЗОБРАЖЕНИЯ
 
 var upload = document.querySelector('#upload-file');
 
 var uploadShow = function () {
-    var filtersWindow = document.querySelector('.img-upload__overlay');
-    filtersWindow.classList.remove('hidden');
-  };
+  var filtersWindow = document.querySelector('.img-upload__overlay');
+  filtersWindow.classList.remove('hidden');
+};
 
 upload.addEventListener('change', uploadShow);
 
+// ФОРМА РЕДАКТИРОВАНИЯ
 
 var RESIZE_STEP = 25;
 var RESIZE_MAX = 100;
 var resize = document.querySelector('.scale__control--value');
 var resizeSmaller = document.querySelector('.scale__control--smaller');
 var resizeBigger = document.querySelector('.scale__control--bigger');
+var preview = document.querySelector('.img-upload__preview');
+
 
 var resizeBiggerHandler = function () {
-    if (parseInt(resize.value, 10) < RESIZE_MAX) {
-      resize.value = parseInt(resize.value, 10) + RESIZE_STEP + '%';
+  if (parseInt(resize.value, 10) < RESIZE_MAX) {
+    resize.value = parseInt(resize.value, 10) + RESIZE_STEP + '%';
+    preview.style.transform = 'scale(0.' + parseInt(resize.value, 10) + ')';
+  } else if (parseInt(resize.value, 10) === RESIZE_MAX) {
+    preview.style.transform = 'none';
   }
 };
 
 var resizeSmallerHandler = function () {
-    if (parseInt(resize.value, 10) < RESIZE_MAX) {
-      resize.value = parseInt(resize.value, 10) - RESIZE_STEP + '%';
+  if (parseInt(resize.value, 10) < RESIZE_MAX) {
+    resize.value = parseInt(resize.value, 10) - RESIZE_STEP + '%';
+    preview.style.transform = 'scale(0.' + parseInt(resize.value, 10) + ')';
   }
 };
 
 resizeBigger.addEventListener('click', resizeBiggerHandler);
 resizeSmaller.addEventListener('click', resizeSmallerHandler);
-
-
