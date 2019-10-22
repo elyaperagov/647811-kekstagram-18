570ebde87cec33c55ce23db954394c6f015a7a39
Елья <piragof@gmail.com>
2019-10-23 03:44:54 +0400

    big picture

--- js/data.js
+++ js/data.js
@@ -34,6 +34,31 @@
     });
   }
 
+
+  // ОТКРЫВАНИЕ И ЗАКРЫВАНИЕ БОЛЬШОЙ КАРТИНКИ
+  var bigPic = document.querySelector('.big-picture');
+
+  var showItem = function (item) {
+    return item.classList.remove('hidden');
+  };
+
+  var hideItem = function (item) {
+    return item.classList.add('hidden');
+  };
+
+  var showBigPhoto = function (data) {
+    showItem(bigPic);
+    bigPic.querySelector('.big-picture__img').querySelector('img').src = data.url;
+  };
+
+  var closeBigPhoto = function () {
+    return bigPic.classList.add('hidden');
+  };
+
+  var closeButton = bigPic.querySelector('#picture-cancel');
+
+
+  // ШАБЛОН
   function renderTemplate(image) {
     var userImage = template.cloneNode(true);
     //  var image = images[i];
@@ -41,6 +66,12 @@
     userImage.querySelector('.picture__img').src = image.url;
     userImage.querySelector('.picture__likes').textContent = image.likes;
     userImage.querySelector('.picture__comments').textContent = image.messages;
+    userImage.addEventListener('click', function () {
+      showBigPhoto(image);
+      closeButton.addEventListener('click', function () {
+        closeBigPhoto(image);
+      });
+    });
 
     return userImage;
   }
@@ -52,10 +83,35 @@
     }
     return fragment;
   }
+  /*
+  var onDataLoad = function (data) {
+    images = data;
+    getImage(images);
+  };
+
+  window.backend.onRequestLoad(onDataLoad, window.backend.onErrorRequest);
+*/
+
+  // СООБЩЕНИЕ ОБ ОШИБКЕ
+  /*  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };*/
+
+  //  window.backend.load(getImage, errorHandler);
+
   window.data = {
     getImage: getImage,
     getRandomNumber: getRandomNumber,
-    renderTemplate: renderTemplate
+    renderTemplate: renderTemplate,
+    hideItem: hideItem
   };
 
 
